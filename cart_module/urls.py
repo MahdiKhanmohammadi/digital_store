39d4382c586ccf8c5edc81e4d73253332cfe6505
MahdiKhanmohammadi <i.mahdikhanmohammadi@gmail.com>
2024-12-26 13:16:35 +0330

    Update urls.py

--- cart_module/urls.py
+++ cart_module/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from cart_module.views import add_product_to_cart,show_cart,remove_product_in_cart,complete_remove,discount_code
+from cart_module.views import add_product_to_cart,show_cart,remove_product_in_cart,complete_remove,discount_code,change_state
 
 urlpatterns = [
     path('add-item',add_product_to_cart,name='add_to_cart'),
     path('remove-item',remove_product_in_cart,name='remove_item'),
     path('remove-item-complete',complete_remove,name='complete_remove'),
     path('',show_cart,name='get_cart'),
-    path('discount_code',discount_code,name='discount_code')
+    path('discount_code',discount_code,name='discount_code'),
+    path('change_state', change_state, name='change_state')
 ]
